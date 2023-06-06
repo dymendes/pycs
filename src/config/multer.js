46f169a0b6ea2979d7ef8fa80ae9dbf9090bbd93
Dymendes <dymealves@gmail.com>
2023-06-06 17:51:21 -0300

    Fix multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,8 +1,13 @@
 import multer from "multer"
 import path from "path"
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, process.env.UPLOADS_SRC),
+  destination: (req, file, cb) => cb(null, `${__dirname}/../uploads`),
   filename: (req, file, cb) => {
     const date = Date.now()
     const extension = path.extname(file.originalname)
