ef54b4ce6c727315a5a95fa720abcb9ca3454230
Dymendes <dymealves@gmail.com>
2023-06-06 17:26:20 -0300

    Fix multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,7 +4,7 @@ import path from "path"
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, process.env.UPLOADS_SRC),
   filename: (req, file, cb) => {
-    const date = new Date().toISOString()
+    const date = Date.now()
     const extension = path.extname(file.originalname)
 
     const filename = `${date}${extension}`
