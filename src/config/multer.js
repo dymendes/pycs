560bda087e772ac6af89d5ae47183fc6d58d9778
Dymendes <dymealves@gmail.com>
2023-06-07 15:44:10 -0300

    Multer fix

--- src/config/multer.js
+++ src/config/multer.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, `${__dirname}/uploads/`),
+  destination: (req, file, cb) => cb(null, "/uploads/"),
   filename: (req, file, cb) => {
     const date = Date.now()
     const extension = path.extname(file.originalname)
