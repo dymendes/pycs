226dd7fed733a9ba43fcd37a6cff42326da256bf
Dymendes <dymealves@gmail.com>
2023-06-23 16:04:23 -0300

    Main endpoint update

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(express.json())
 app.use("/file", express.static(path.resolve(__dirname, "uploads")))
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Pycs api!" })
+  res.json({ message: "Welcome to Pycs!" })
 })
 
 app.use("/user", UserRouter)
